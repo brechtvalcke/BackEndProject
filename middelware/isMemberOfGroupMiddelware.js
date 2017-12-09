642e0dac2720b8b02b3e50fdbd80598e013bd7c1
thuur vandeplassche <thuur@protoware.be>
2017-12-09 23:17:27 +0100

    middleware fix

--- middelware/isMemberOfGroupMiddelware.js
+++ middelware/isMemberOfGroupMiddelware.js
@@ -1,34 +1,35 @@
 const GroupService = require("./../service/GroupService");
 const groupService = new GroupService();
-module.exports = function(req,res,next) {
+module.exports = function (req, res, next) {
     let groupId;
-    if(req.params.groupId){
+
+    if (req.params.groupId) {
         groupId = req.params.groupId;
-    }else if(req.body.groupId) {
+    } else if (req.body.groupId) {
         groupId = req.body.groupId;
-    }else{
+    } else {
         res.status(400);
-        res.json({error:"groupId not set"});
+        res.json({error: "groupId not set"});
     }
     groupService.getGroup(groupId)
-    .then(group => {
-        let memberOfGroup = false;
-        group.users.forEach(function(user) {
+        .then(group => {
+            let memberOfGroup = false;
+            group[0].users.forEach(function (user) {
 
-            if(user._id === req.user.data._id){
-                memberOfGroup = true;
+                if (user._id === req.user.data._id) {
+                    memberOfGroup = true;
+                }
+            }, this);
+            if (memberOfGroup) {
+                next();
+            } else {
+                res.status(403);
+                res.json({error: "Not a member of the group"});
             }
-        }, this);
-        if(memberOfGroup){
-            next();
-        }else{
-            res.status(403);
-            res.json({error:"Not a member of the group"});
-        }
-    })
-    .catch(error => {
-        res.status(400);
-        console.log(error);
-        res.json({error:"Error fetching group"});
-    })
+        })
+        .catch(error => {
+            res.status(400);
+            console.log(error);
+            res.json({error: "Error fetching group"});
+        })
 }
