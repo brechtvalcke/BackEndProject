2b295b93d25686fe8f905f96164a40a92caf3eb4
thuur vandeplassche <thuur@protoware.be>
2018-01-04 14:42:38 +0100

    performance update

--- service/GroupService.js
+++ service/GroupService.js
@@ -3,20 +3,21 @@ let GroupModel = require("../model/GroupModel");
 let UserService = require("../service/UserService");
 
 module.exports = class GroupService {
-    constructor(io){
+    constructor(io) {
         this.io = io;
         this.groupRepository = new GroupRepository();
         this.userService = new UserService();
     }
 
-    getGroups(userID){
+    getGroups(userID) {
         return new Promise((resolve, reject) => {
             this.groupRepository.getGroups(userID)
                 .then(result => resolve(result))
                 .catch(error => reject(error));
         });
     }
-    getInvites(userID){
+
+    getInvites(userID) {
         return new Promise((resolve, reject) => {
             this.groupRepository.getInvites(userID)
                 .then(result => resolve(result))
@@ -24,22 +25,31 @@ module.exports = class GroupService {
         });
     }
 
-    acceptInvite(groupID,userID){
-        return new Promise((resolve,reject) => {
-           this.groupRepository.acceptInvite(groupID, userID)
-               .then(res => { resolve(res); })
-               .catch(err => { reject(err); })
-        });
-    }
-    declineInvite(groupID,userID){
-        return new Promise((resolve,reject) => {
-            this.groupRepository.declineInvite(groupID, userID)
-                .then(res => { resolve(res); })
-                .catch(err => { reject(err); })
+    acceptInvite(groupID, userID) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.acceptInvite(groupID, userID)
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                })
         });
     }
 
-    getGroup(groupID){
+    declineInvite(groupID, userID) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.declineInvite(groupID, userID)
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                })
+        });
+    }
+
+    getGroup(groupID) {
         return new Promise((resolve, reject) => {
             this.groupRepository.getGroup(groupID)
                 .then(result => resolve(result))
@@ -48,11 +58,11 @@ module.exports = class GroupService {
         });
     }
 
-    createGroup(groupToCreate){
+    createGroup(groupToCreate) {
         return new Promise((resolve, reject) => {
             let promises = [
                 this.groupRepository.createGroup(groupToCreate),
-                this.sendInviteToFacebookFriends(groupToCreate.createBy,groupToCreate.users)
+                this.sendInviteToFacebookFriends(groupToCreate.createBy, groupToCreate.users)
             ];
 
             // TODO call facebook API to send invite
@@ -67,110 +77,99 @@ module.exports = class GroupService {
                         console.log(error);
                         reject(error);
                     });
-            }catch (error){
+            } catch (error) {
                 console.error(error);
                 reject(error);
             }
         });
     }
 
-    sendInviteToFacebookFriends(userID,friendsArray) {
+    sendInviteToFacebookFriends(userID, friendsArray) {
         return new Promise((resolve, reject) => {
-            friendsArray.forEach(friend => {
-                this.userService.getUserById(userID)
-                    .then(user => this.io.sockets.to(friend._id).emit("inviteNotification",user.name))
-                    .catch(error => reject(error));
-            });
+            this.userService.getUserById(userID)
+                .then(user => {
+                    friendsArray.forEach(friend => {
+                        this.io.sockets.to(friend._id).emit("inviteNotification", user.name)
+                    })
+                })
+                .catch(error => reject(error));
+
+
             //this.io.emit("inviteNotification",userID);
             resolve(true);
         });
     }
-    updateGroupName(newName,groupID) {
+
+    updateGroupName(newName, groupID) {
         return new Promise((resolve, reject) => {
-            let nameUpdateJson = {name:newName};
-            this.groupRepository.updateGroupName(nameUpdateJson,groupID)
-            .then(result => {
-                resolve(result);
-            })
-            .catch(error => {
-                reject(error);
-            })
-        });
-    }
-    getAllActivitiesForGroup(groupID){
-        return new Promise((resolve, reject) => {
-            this.getGroup(groupID)
-            .then(group => {
-                resolve(group.activity);
-            })
-            .catch(error => {
-                reject(error);
-            })
-        });
-    }
-    addActivityForGroup(body,groupID,userID){
-        return new Promise((resolve, reject) => {
-            const activity = {
-                name: body.name,
-                users: [{userID:userID,accepted: true}]
-            };
-            this.groupRepository.addActivityForGroup(activity,groupID)
-            .then(result => {
-                this.getLastAddedActivity(groupID,activity.name)
-                    .then(result => { resolve(result)})
-                    .catch(error => { reject(error); })
-            })
-            .catch(error => {
-                reject(error);
-            })
+            let nameUpdateJson = {name: newName};
+            this.groupRepository.updateGroupName(nameUpdateJson, groupID)
+                .then(result => {
+                    resolve(result);
+                })
+                .catch(error => {
+                    reject(error);
+                })
         });
     }
 
-    getLastAddedActivity(groupID,activityName){
+    getAllActivitiesForGroup(groupID) {
         return new Promise((resolve, reject) => {
-            this.groupRepository.getLastAddedActivity(groupID,activityName)
+            this.getGroup(groupID)
+                .then(group => {
+                    resolve(group.activity);
+                })
+                .catch(error => {
+                    reject(error);
+                })
+        });
+    }
+
+    addActivityForGroup(body, groupID, userID) {
+        return new Promise((resolve, reject) => {
+            const activity = {
+                name: body.name,
+                users: [{userID: userID, accepted: true}]
+            };
+            this.groupRepository.addActivityForGroup(activity, groupID)
+                .then(result => {
+                    this.getLastAddedActivity(groupID, activity.name)
+                        .then(result => {
+                            resolve(result)
+                        })
+                        .catch(error => {
+                            reject(error);
+                        })
+                })
+                .catch(error => {
+                    reject(error);
+                })
+        });
+    }
+
+    getLastAddedActivity(groupID, activityName) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.getLastAddedActivity(groupID, activityName)
                 .then(result => {
                     resolve(result[0].activity[result[0].activity.length - 1]);
                 })
-                .catch(error => { console.log(error);reject(error); })
+                .catch(error => {
+                    console.log(error);
+                    reject(error);
+                })
         });
     }
 
-    updateActivityInGroup(body,groupID){
+    updateActivityInGroup(body, groupID) {
         return new Promise((resolve, reject) => {
             const activity = {
-                _id:body._id,
+                _id: body._id,
                 name: body.name,
                 users: body.users,
             };
-            this.groupRepository.updateActivityInGroup(activity,groupID)
-            .then(result => {
-                resolve(result);
-            })
-            .catch(error => {
-                reject(error);
-            })
-        });
-    }
-    getAllTimeslotsForGroup(groupID){
-        return new Promise((resolve, reject) => {
-            this.getGroup(groupID)
-            .then(group => {
-                resolve(group.timeSlot);
-            })
-            .catch(error => {
-                reject(error);
-            })
-        });
-    }
-
-    addTimeslotForGroup(body,groupID){
-        return new Promise((resolve,reject) => {
-            this.groupRepository.addTimeslotForGroup(body,groupID)
+            this.groupRepository.updateActivityInGroup(activity, groupID)
                 .then(result => {
-                    this.getLastAddedTimeslot(groupID,body)
-                        .then(result => { resolve(result) })
-                        .catch(error => { reject(error); })
+                    resolve(result);
                 })
                 .catch(error => {
                     reject(error);
@@ -178,19 +177,52 @@ module.exports = class GroupService {
         });
     }
 
-    getLastAddedTimeslot(groupID,timeSlot){
+    getAllTimeslotsForGroup(groupID) {
         return new Promise((resolve, reject) => {
-            this.groupRepository.getLastAddedTimeslot(groupID,timeSlot)
+            this.getGroup(groupID)
+                .then(group => {
+                    resolve(group.timeSlot);
+                })
+                .catch(error => {
+                    reject(error);
+                })
+        });
+    }
+
+    addTimeslotForGroup(body, groupID) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.addTimeslotForGroup(body, groupID)
+                .then(result => {
+                    this.getLastAddedTimeslot(groupID, body)
+                        .then(result => {
+                            resolve(result)
+                        })
+                        .catch(error => {
+                            reject(error);
+                        })
+                })
+                .catch(error => {
+                    reject(error);
+                })
+        });
+    }
+
+    getLastAddedTimeslot(groupID, timeSlot) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.getLastAddedTimeslot(groupID, timeSlot)
                 .then(result => {
                     resolve(result[0].timeSlot[result[0].timeSlot.length - 1]);
                 })
-                .catch(error => { console.log(error);reject(error); })
+                .catch(error => {
+                    console.log(error);
+                    reject(error);
+                })
         });
     }
 
-    voteForTimeSlotInGroup(groupID,timeSlotID,userID){
-        return new Promise((resolve,reject) => {
-            this.groupRepository.voteForTimeSlotInGroup(groupID,timeSlotID,userID)
+    voteForTimeSlotInGroup(groupID, timeSlotID, userID) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.voteForTimeSlotInGroup(groupID, timeSlotID, userID)
                 .then(result => {
                     resolve(result);
                 })
@@ -199,21 +231,22 @@ module.exports = class GroupService {
                 })
         });
     }
-    removeVoteForTimeSlotInGroup(groupID,timeSlotID,userID){
-        return new Promise((resolve,reject) => {
-            this.groupRepository.removeVoteForTimeSlotInGroup(groupID,timeSlotID,userID)
-            .then(result => {
-                resolve(result);
-            })
-            .catch(error => {
-                reject(error);
-            })
+
+    removeVoteForTimeSlotInGroup(groupID, timeSlotID, userID) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.removeVoteForTimeSlotInGroup(groupID, timeSlotID, userID)
+                .then(result => {
+                    resolve(result);
+                })
+                .catch(error => {
+                    reject(error);
+                })
         })
     }
 
-    voteForActivityInGroup(groupID,activityID,userID){
-        return new Promise((resolve,reject) => {
-            this.groupRepository.voteForActivityInGroup(groupID,activityID,userID)
+    voteForActivityInGroup(groupID, activityID, userID) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.voteForActivityInGroup(groupID, activityID, userID)
                 .then(result => {
                     resolve(result);
                 })
@@ -222,62 +255,66 @@ module.exports = class GroupService {
                 })
         });
     }
-    removeVoteForActivityInGroup(groupID,activityID,userID) {
-        return new Promise((resolve,reject) => {
-            this.groupRepository.removeVoteForActivityInGroup(groupID,timeSlotID,userID)
-            .then(result => {
-                resolve(result);
-            })
-            .catch(error => {
-                reject(error);
-            })
+
+    removeVoteForActivityInGroup(groupID, activityID, userID) {
+        return new Promise((resolve, reject) => {
+            this.groupRepository.removeVoteForActivityInGroup(groupID, timeSlotID, userID)
+                .then(result => {
+                    resolve(result);
+                })
+                .catch(error => {
+                    reject(error);
+                })
         })
     }
-    sendMessage(groupID,message) {
-        return new Promise((resolve,reject) => {
 
-            this.groupRepository.addMessageToGroup(groupID,message)
-            .then(result => {
-                resolve(result);
-            })
-            .catch(error => {
-                reject(error);
-            });
+    sendMessage(groupID, message) {
+        return new Promise((resolve, reject) => {
+
+            this.groupRepository.addMessageToGroup(groupID, message)
+                .then(result => {
+                    resolve(result);
+                })
+                .catch(error => {
+                    reject(error);
+                });
 
         });
     }
+
     getMessagesByGroupID(groupID) {
         return new Promise((resolve, reject) => {
             this.groupRepository.getMessagesByGroupID(groupID)
-            .then(res => {
-                resolve(res);
-            })
-            .catch(err => {
-                reject(err);
-            })
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                })
         });
     }
 
-    getUserObjectsByGroupId(groupID){
-        return new Promise((resolve,reject) => {
-            
+    getUserObjectsByGroupId(groupID) {
+        return new Promise((resolve, reject) => {
+
             this.groupRepository.getUsersInGroup(groupID)
-            .then(usersInGroup=>{
-                let Promises = [];
-                usersInGroup.forEach(userData => {
-                    Promises.push(this.userService.getUserById(userData._id));
-                    
-                });
-                Promise.all(Promises).then(res => {
-                    let Users = [];
-                    res.forEach(user => {
-                        Users.push(user);
+                .then(usersInGroup => {
+                    let Promises = [];
+                    usersInGroup.forEach(userData => {
+                        Promises.push(this.userService.getUserById(userData._id));
+
                     });
-                    resolve(Users);
+                    Promise.all(Promises).then(res => {
+                        let Users = [];
+                        res.forEach(user => {
+                            Users.push(user);
+                        });
+                        resolve(Users);
+                    })
+                        .catch(error => {
+                        });
                 })
-                .catch(error => {});
-            })
-            .catch(error => reject(error));
+                .catch(error => reject(error));
         });
     }
 };
